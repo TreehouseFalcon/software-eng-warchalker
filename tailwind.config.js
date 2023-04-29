fbc459c2b43379278baea32eff89dce01370b8a0
Juniper Hovey <me@juniper.link>
2023-04-29 13:15:12 -0500

    Init commit

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [],
+  theme: {
+    extend: {},
+    fontFamily: {
+      sans: ["Inter", "Roboto", "sans-serif"],
+      code: ["Consolas"],
+    },
+  },
+  plugins: [],
+};
